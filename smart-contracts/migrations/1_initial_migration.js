ad6aa66aa5d4220048d19bc50b425766db156e85
Amit Shah <amitshah0t7@gmail.com>
2018-04-15 17:51:24 -0500

    add approval for gotoken transfer for fees when creating channel and seperate erc20 token for actual value transfer in the state channel

--- smart-contracts/migrations/1_initial_migration.js
+++ smart-contracts/migrations/1_initial_migration.js
@@ -6,7 +6,7 @@ var ChannelManagerLibrary = artifacts.require("./ChannelManagerLibrary.sol");
 var NettingChannelContract = artifacts.require("./NettingChannelContract.sol");
 
 var NettingChannelLibrary = artifacts.require("./NettingChannelLibrary.sol");
-var Registry = artifacts.require("./Registry.sol");
+//var Registry = artifacts.require("./Registry.sol");
 var Utils = artifacts.require("./Utils.sol");
 
 var web3 = require('Web3');
@@ -19,51 +19,58 @@ module.exports = function(deployer) {
   	deployer.deploy(StandardToken).then(function(){
 
       deployer.deploy(HumanStandardToken, 100000000,"GoNetwork",1,"$GOT").then(function(){
+        var gotToken = HumanStandardToken.at(HumanStandardToken.address);
+        var token = null;
+         HumanStandardToken.new(50000, "TEST_TOKEN", 1, "$NO",{from:acct2}).then(function(instance){
+          token = instance;
+           console.log("ERC20 TOKEN \r\n\r\n");
+                  console.log(token.address);
+            console.log("\r\n\r\n=======================\r\n\r\n");
 
         deployer.link(HumanStandardToken, NettingChannelLibrary);
         deployer.deploy(NettingChannelLibrary).then(function() {
-
           deployer.link(NettingChannelLibrary,NettingChannelContract);
-
-          deployer.link(NettingChannelLibrary, ChannelManagerLibrary);
-          //deployer.link(NettingChannelContract, ChannelManagerLibrary);
-          
+          deployer.link(NettingChannelLibrary, ChannelManagerLibrary);          
           deployer.deploy(ChannelManagerLibrary).then(function() {
             deployer.link(ChannelManagerLibrary, ChannelManagerContract);
-
-
-            deployer.deploy(ChannelManagerContract, HumanStandardToken.address).then(function(){
+            deployer.deploy(ChannelManagerContract, gotToken.address, token.address).then(function(){
 
               ChannelManagerContract.deployed().then(function(instance) {
                
-                var hst = HumanStandardToken.at(HumanStandardToken.address);
-                hst.approve(ChannelManagerContract.address,500, {from:acct1}).then(function(){
+                gotToken.approve(ChannelManagerContract.address,500, {from:acct1}).then(function(){
                   instance.newChannel(acct2, 150, {from:acct1}).then(function (result) {
+                  console.log("NEW CHANNEL RESULT \r\n\r\n");
                   console.log(result);
+                  console.log("\r\n\r\n=======================\r\n\r\n");
+                  if(result.logs[0].event !== "ChannelNew"){
+                    throw new Error("COULD NOT CREATE NEW CHANNEL");
+                  }
                   instance.getChannelWith(acct2,{from:acct1}).then(function(channelAddress){
                     console.log(channelAddress);
                     var ncc = NettingChannelContract.at(channelAddress);
 
-                    HumanStandardToken.deployed().then(function(hst) {
 
-                      hst.approve(ncc.address,500,{from:acct1}).then(function (next) {
-                        ncc.deposit(250,{from:acct1}).then(function (result) {
-                          console.log("DEPOSITED 250");
-                          // body...
-                        }).catch(function (err) {
-                          conole.log("COULD NOT DEPOST 250");
-                          // body...
-                        });
-                        // body...
-                      })
-                    })
+                    token.approve(ncc.address,400,{from:acct2}).then(function (next) {
+                      ncc.deposit(250,{from:acct2}).then(function (result) {
+                           console.log(" CHANNEL DEPOSIT RESULT \r\n\r\n");
+                            console.log(result);
+                            console.log("\r\n\r\n=======================\r\n\r\n");
+
+                      }).catch(function (err) {
+                          console.log("COULD NOT DEPOST 250");
+                         
+                      });
+                    }).catch(function(err) {
+                      console.log("COULD NOT APPROVE TOKEN ");
+                    })//approve token
+                    
                   }).catch(function (e) {
-                    // body...
+                    
                   })
                 }).catch(function (e) {
-                  console.log("ERROR"+e.message);
-                })
-                  
+                  console.log("ERROR Creating New Channel:"+e.message);
+                })//New Channel
+
                 })
 
                 
@@ -76,10 +83,10 @@ module.exports = function(deployer) {
         })//CHannelManagerLibrary
 
         
-      });
+        });//NettingChannelLibrary
 
-
-      })
+        })//ERC-20 Random token
+      })//Gonetwork TOken
 
       
     
