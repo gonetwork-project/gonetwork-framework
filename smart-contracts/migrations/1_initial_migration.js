ae372f402163ad5b3e0e51b338d1cc3f515da7da
Amit Shah <amitshah0t7@gmail.com>
2018-04-16 16:14:43 -0500

    newCHannel, close(no proof), deposit, token-approve, settle tested

--- smart-contracts/migrations/1_initial_migration.js
+++ smart-contracts/migrations/1_initial_migration.js
@@ -11,11 +11,16 @@ var Utils = artifacts.require("./Utils.sol");
 
 var web3 = require('Web3');
 
+//testrpc -g 20  --unlock 0 --unlock 1 --unlock 2 --unlock 3 --account="0xb507928218b7b1e48f82270011149c56b6191cd1f2846e01c419f0a1a57acc42,10000000000000000000000000" --account="0x4c65754b227fb8467715d2949555abf6fe8bcba11c6773433c8a7a05a2a1fc78,10000000000000000000000000" --account="0xa8344e81509696058a3c14e520693f94ce9c99c26f03310b2308a4c59b35bb3d,10000000000000000000000000" --account="0x157258c195ede5fad2f054b45936dae4f3e1b1f0a18e0edc17786d441a207224,10000000000000000000000000"
+
+
 module.exports = function(deployer) {
 	console.log("Deploying Standard Token");
-    var acct1 = "0x7e24f888e631f0ff32f030a361a3bb0e0026a216";
-    var acct2 = "0xe415cae97729d38d2156b2b3d628e0f8f61f8aac";
-    var acct3 = "0xe9a476b0dfb0dfb93fc6bfed1a97b34689e4c032";
+    var acct1 = "0xf0c3043550e5259dc1c838d0ea600364d999ea15";
+    var acct2 = "0xb0ae572146ab8b5990e069bff487ac25635dabe8";
+    var acct3 = "0xff8a018d100ace078d214f02be8df9c6944e7a2b";
+    var acct4 = "0xf77e9ef93380c70c938ca2e859baa88be650d87d";
+   
   	deployer.deploy(StandardToken).then(function(){
 
       deployer.deploy(HumanStandardToken, 100000000,"GoNetwork",1,"$GOT").then(function(){
@@ -23,10 +28,15 @@ module.exports = function(deployer) {
         var token = null;
          HumanStandardToken.new(50000, "TEST_TOKEN", 1, "$NO",{from:acct2}).then(function(instance){
           token = instance;
+          
+
            console.log("ERC20 TOKEN \r\n\r\n");
                   console.log(token.address);
             console.log("\r\n\r\n=======================\r\n\r\n");
-
+            //transfer some money to account1
+          token.transfer(acct1, 25000,{from:acct2}).then(function(res) {
+            console.log(res);
+          });
         deployer.link(HumanStandardToken, NettingChannelLibrary);
         deployer.deploy(NettingChannelLibrary).then(function() {
           deployer.link(NettingChannelLibrary,NettingChannelContract);
