865773f1d6b44023d4ecfda6842372e5e6e1668a
Amit Shah <amitshah0t7@gmail.com>
2018-04-20 18:55:01 -0500

    add ropsten testnet in truffle config

--- smart-contracts/truffle.js
+++ smart-contracts/truffle.js
@@ -4,9 +4,15 @@ module.exports = {
   networks: {
    development: {
 	   host: "localhost",
-	   port: 8545,
+	   port: 8000,
 	   network_id: "*", // Match any network id
 	   gas: 6721975
 	  }
-	}
+	,
+	ropsten: {
+      host: "localhost",
+      port: 8545,
+      network_id: "3",
+      gas:3500000
+    }}
 };
