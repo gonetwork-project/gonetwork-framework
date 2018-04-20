f66c3259e8bd72f842a0a2493737ea58f064b139
Amit Shah <amitshah0t7@gmail.com>
2018-04-20 18:56:13 -0500

    simplify the amount of configuration completed by truffle migration

--- smart-contracts/migrations/1_initial_migration.js
+++ smart-contracts/migrations/1_initial_migration.js
@@ -21,92 +21,120 @@ module.exports = function(deployer) {
     var acct3 = "0xff8a018d100ace078d214f02be8df9c6944e7a2b";
     var acct4 = "0xf77e9ef93380c70c938ca2e859baa88be650d87d";
    
-  	deployer.deploy(StandardToken).then(function(){
 
-      deployer.deploy(HumanStandardToken, 100000000,"GoNetwork",1,"$GOT").then(function(){
-        var gotToken = HumanStandardToken.at(HumanStandardToken.address);
-        var token = null;
-         HumanStandardToken.new(50000, "TEST_TOKEN", 1, "$NO",{from:acct2}).then(function(instance){
-          token = instance;
+  deployer.then(async () => {
+    await deployer.deploy(StandardToken)
+    await deployer.link(StandardToken, HumanStandardToken);
+    await deployer.deploy(HumanStandardToken, 100000000,"GoNetwork",1,"$GOT");
+    var gotToken= await HumanStandardToken.deployed();
+    console.log("GOT ADDRESS:"+gotToken.address.toString('hex'));
+
+    var testToken = await HumanStandardToken.new(50000,"TEST_TOKEN",1, "$NO", {from:acct2});
+    console.log("TEST ERC20:" + testToken.address.toString('hex') );
+    //await testToken.transfer(acct1, 25000,{from:acct2});
+
+    await  deployer.link(HumanStandardToken, NettingChannelLibrary);
+    await deployer.deploy(NettingChannelLibrary);
+    await deployer.link(NettingChannelLibrary,NettingChannelContract);
+    await deployer.link(NettingChannelLibrary, ChannelManagerLibrary);   
+    await deployer.deploy(ChannelManagerLibrary);
+    await deployer.link(ChannelManagerLibrary, ChannelManagerContract);
+    await deployer.deploy(ChannelManagerContract, gotToken.address, testToken.address);
+    
+    
+
+
+   // deployer.deploy(StandardToken).then(function(success){
+   //  console.log(success)
+   // }).catch(function(error){
+   //  console.log(error);
+   // })
+  	// deployer.deploy(StandardToken).then(function(){
+
+   //    deployer.deploy(HumanStandardToken, 100000000,"GoNetwork",1,"$GOT").then(function(){
+   //      var gotToken = HumanStandardToken.at(HumanStandardToken.address);
+   //      var token = null;
+   //       HumanStandardToken.new(50000, "TEST_TOKEN", 1, "$NO",{from:acct2}).then(function(instance){
+   //        token = instance;
           
 
-           console.log("ERC20 TOKEN \r\n\r\n");
-                  console.log(token.address);
-            console.log("\r\n\r\n=======================\r\n\r\n");
-            //transfer some money to account1
-          token.transfer(acct1, 25000,{from:acct2}).then(function(res) {
-            console.log(res);
-          });
-        deployer.link(HumanStandardToken, NettingChannelLibrary);
-        deployer.deploy(NettingChannelLibrary).then(function() {
-          deployer.link(NettingChannelLibrary,NettingChannelContract);
-          deployer.link(NettingChannelLibrary, ChannelManagerLibrary);          
-          deployer.deploy(ChannelManagerLibrary).then(function() {
-            deployer.link(ChannelManagerLibrary, ChannelManagerContract);
-            deployer.deploy(ChannelManagerContract, gotToken.address, token.address).then(function(){
+   //         console.log("ERC20 TOKEN \r\n\r\n");
+   //                console.log(token.address);
+   //          console.log("\r\n\r\n=======================\r\n\r\n");
+   //          //transfer some money to account1
+   //        token.transfer(acct1, 25000,{from:acct2}).then(function(res) {
+   //          console.log(res);
+   //        });
+   //      deployer.link(HumanStandardToken, NettingChannelLibrary);
+   //      deployer.deploy(NettingChannelLibrary).then(function() {
+   //        deployer.link(NettingChannelLibrary,NettingChannelContract);
+   //        deployer.link(NettingChannelLibrary, ChannelManagerLibrary);          
+   //        deployer.deploy(ChannelManagerLibrary).then(function() {
+   //          deployer.link(ChannelManagerLibrary, ChannelManagerContract);
+   //          deployer.deploy(ChannelManagerContract, gotToken.address, token.address).then(function(){
 
-              ChannelManagerContract.deployed().then(function(instance) {
+   //            ChannelManagerContract.deployed().then(function(instance) {
                
-                gotToken.approve(ChannelManagerContract.address,500, {from:acct1}).then(function(){
-                  instance.newChannel(acct2, 150, {from:acct1}).then(function (result) {
-                  console.log("NEW CHANNEL RESULT \r\n\r\n");
-                  console.log(result);
-                  console.log("\r\n\r\n=======================\r\n\r\n");
-                  if(result.logs[0].event !== "ChannelNew"){
-                    throw new Error("COULD NOT CREATE NEW CHANNEL");
-                  }
-                  instance.getChannelWith(acct2,{from:acct1}).then(function(channelAddress){
-                    console.log(channelAddress);
-                    var ncc = NettingChannelContract.at(channelAddress);
+   //              gotToken.approve(ChannelManagerContract.address,500, {from:acct1}).then(function(){
+   //                instance.newChannel(acct2, 150, {from:acct1}).then(function (result) {
+   //                console.log("NEW CHANNEL RESULT \r\n\r\n");
+   //                console.log(result);
+   //                console.log("\r\n\r\n=======================\r\n\r\n");
+   //                if(result.logs[0].event !== "ChannelNew"){
+   //                  throw new Error("COULD NOT CREATE NEW CHANNEL");
+   //                }
+   //                instance.getChannelWith(acct2,{from:acct1}).then(function(channelAddress){
+   //                  console.log(channelAddress);
+   //                  var ncc = NettingChannelContract.at(channelAddress);
 
 
-                    token.approve(ncc.address,400,{from:acct2}).then(function (next) {
-                      ncc.deposit(250,{from:acct2}).then(function (result) {
-                           console.log(" CHANNEL DEPOSIT RESULT \r\n\r\n");
-                            console.log(result);
-                            console.log("\r\n\r\n=======================\r\n\r\n");
+   //                  token.approve(ncc.address,400,{from:acct2}).then(function (next) {
+   //                    ncc.deposit(250,{from:acct2}).then(function (result) {
+   //                         console.log(" CHANNEL DEPOSIT RESULT \r\n\r\n");
+   //                          console.log(result);
+   //                          console.log("\r\n\r\n=======================\r\n\r\n");
 
-                      }).catch(function (err) {
-                          console.log("COULD NOT DEPOST 250");
+   //                    }).catch(function (err) {
+   //                        console.log("COULD NOT DEPOST 250");
                          
-                      });
-                    }).catch(function(err) {
-                      console.log("COULD NOT APPROVE TOKEN ");
-                    })//approve token
+   //                    });
+   //                  }).catch(function(err) {
+   //                    console.log("COULD NOT APPROVE TOKEN ");
+   //                  })//approve token
                     
-                  }).catch(function (e) {
+   //                }).catch(function (e) {
                     
-                  })
-                }).catch(function (e) {
-                  console.log("ERROR Creating New Channel:"+e.message);
-                })//New Channel
+   //                })
+   //              }).catch(function (e) {
+   //                console.log("ERROR Creating New Channel:"+e.message);
+   //              })//New Channel
 
-                instance.newChannel(acct3, 150, {from:acct1}).then(function (result) {
-                }).catch(function(err) {
-                  console.log("ERROR Creating New Channel with Acct3:"+ e.message);
-                })
+   //              instance.newChannel(acct3, 150, {from:acct1}).then(function (result) {
+   //              }).catch(function(err) {
+   //                console.log("ERROR Creating New Channel with Acct3:"+ e.message);
+   //              })
 
-                })
+   //              })
 
                 
                 
-              });
+   //            });
               
 
-            });
+   //          });
             
-        })//CHannelManagerLibrary
+   //      })//CHannelManagerLibrary
 
         
-        });//NettingChannelLibrary
+   //      });//NettingChannelLibrary
 
-        })//ERC-20 Random token
-      })//Gonetwork TOken
+   //      })//ERC-20 Random token
+   //    })//Gonetwork TOken
 
       
     
 
-    });
+   });
 
   	
   	
