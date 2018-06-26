971108e64e2b9dc46a9758231abfb4a0026cd570
artur <artur@unao.pl>
2018-06-26 15:28:42 +0200

    remove unused types

--- src/types/contracts.ts
+++ src/types/contracts.ts
@@ -3,32 +3,6 @@ import { ChannelEvents, ChannelEventsToArgs } from '../__GEN__/NettingChannelCon
 import { ManagerEvents, ManagerEventsToArgs } from '../__GEN__/ChannelManagerContract'
 import { TokenEvents, TokenEventsToArgs } from '../__GEN__/HumanStandardToken'
 
-export type Method<In, Out> = [In, Out]
-
-export interface ContractBase {
-  [k: string]: Method<any, any>
-}
-
-export type ContractInOrder<CB extends ContractBase> = {
-  [K in keyof CB]: Array<keyof CB[K][0]>
-}
-
-export type ContractOut<CB extends ContractBase> = {
-  [K in keyof CB]: CB[K][1]
-}
-
-export type CreateTxParams<In extends ({} | null), Out> = (provided: TxParamsRequired & TxParamsWithGas) => (params: In) => Out
-
-export type FunctionCall<T extends { [K: string]: [any, any] }, Out> = {
-  [K in keyof T]: CreateTxParams<T[K][0], Out>
-}
-
-export type CreateTxConstParams<In extends ({} | null), Out> = (provided: TxParamsRequired) => (params: In) => Out
-
-export type FunctionConstCall<T extends { [K: string]: [any, any] }, Out> = {
-  [K in keyof T]: CreateTxConstParams<T[K][0], Out>
-}
-
 export type TxSendRaw<T extends { [K: string]: [any, any] }> = {
   [K in keyof T]: T[K][0] extends null ?
   (params: TxParamsRequired & Partial<TxParamsWithGas>) => Promise<T[K][1]> :
