daf9e70cb251aad287b17de9049057818d0642d6
artur <artur@unao.pl>
2018-07-02 22:24:28 +0200

    intercept eth_getLogs calls in tests and allow setting monitoring interval

--- src/tests/setup.ts
+++ src/tests/setup.ts
@@ -6,9 +6,10 @@ import { fakeStorage, CHAIN_ID } from '../utils'
 import { Payload } from '../p2p/p2p-types'
 import { readFromDisk as c } from './init-contracts'
 
-import * as cfg from './config'
+import * as cfgBase from './config'
 
-export const setupClient = (accountIndex: number) => {
+export const setupClient = (accountIndex: number, config?: Partial<typeof cfgBase>) => {
+  const cfg = Object.assign({}, cfgBase, config)
   const account = cfg.accounts[accountIndex]
   if (!account) throw new Error('NO ACCOUNT FOUND')
 
@@ -25,8 +26,21 @@ export const setupClient = (accountIndex: number) => {
     chainId: CHAIN_ID.GETH_PRIVATE_CHAINS,
     owner: account.address,
     signatureCb: (cb) => cb(account.privateKey),
-    providerUrl: cfg.rpcUrl
+    providerUrl: cfg.rpcUrl,
+    monitoringInterval: cfg.monitoringInterval
   })
+
+  // seems that ganache-cli always return empty array if address set
+  // the other problem seems to be that fromBlock and toBlock are ignored
+  // todo: investigate it further
+  const getLogs = blockchain.rpc.getLogs;
+  (blockchain.rpc.getLogs as any) = (ps) => {
+    return getLogs({ fromBlock: ps.fromBlock, toBlock: ps.toBlock })
+      .then(logs => {
+        console.log('LOGS-COUNT', accountIndex, logs.length)
+        return logs
+      })
+  }
 
   const engine = new Engine({
     address: account.address,
