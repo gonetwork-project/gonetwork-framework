be1b5944df3b922de3a5e0c617a8f8cb7b412230
artur <artur@unao.pl>
2018-07-03 19:26:02 +0200

    remove hacky solution for dealing with broken ganache

--- src/tests/setup.ts
+++ src/tests/setup.ts
@@ -36,25 +36,6 @@ export const setupClient = (accountIndex: number, config?: Partial<typeof cfgBas
     monitoringInterval: cfg.monitoringInterval
   })
 
-  // ganache-cli always return empty array if addresses are properly encoded (`0x[20Bytes]`)
-  // another issue is that if no address present fromBlock and toBlock seem to be ignored
-  // we could consider fixing it properly:
-  // https://github.com/trufflesuite/ganache-core/blob/develop/lib/statemanager.js#L643
-  const getLogs = blockchain.rpc.getLogs;
-  (blockchain.rpc.getLogs as any) = (ps: LogsParams) => {
-    return getLogs({
-      fromBlock: ps.fromBlock, toBlock: ps.toBlock,
-      address: ps.address
-      // address: (ps as any).address.map(a =>
-      //   a.toString('hex'))
-    })
-      .then(logs => {
-        console.log(`LOGS acc: ${accountIndex}, len: ${logs.length}, ${ps.fromBlock} - ${ps.toBlock}`)
-        logs.length && console.log(logs)
-        return logs
-      })
-  }
-
   const engine = new Engine({
     address: account.address,
     sign: (msg) => msg.sign(account.privateKey),
