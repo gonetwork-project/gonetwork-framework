582b66e0269d3ac438ce3f7492f093e93049996b
unao <artur@unao.pl>
2018-07-18 15:00:16 +0200

    switch to official ganache-core, allow blockTime to ganache server, update package-lock

--- src/tests/ganache-server.ts
+++ src/tests/ganache-server.ts
@@ -1,18 +1,15 @@
 import * as G from 'ganache-core'
 import * as cfg from './config'
 
-export const serve = (c: Partial<{ blockTime: number, port: number }> = {}) => {
+export const serve = () => {
   const options = {
-    port: c.port || 8546,
+    blockTime: process.argv.length === 3 ? parseFloat(process.argv.length[2]) : 0.05,  // seconds (not milliseconds)
+    port: 8546,
     hostname: 'localhost',
     mnemonic: cfg.mnemonic,
     default_balance_ether: 10000,
-    blockTime: c.blockTime || 2 || 0.05, // todo: support via param
     gasPrice: 200,
-    logger: console,
-    unlocked_accounts: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
-    secure: false,
-    network_id: '*'
+    logger: console
   }
   const srv = new G.server(options)
   srv.listen(options.port, options.hostname, (err) => {
