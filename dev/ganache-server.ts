bcfc8345feaf8a1aac2c702df7a4845dc0c06a6d
unao <artur@unao.pl>
2018-07-25 14:51:37 +0200

    dev: start with just 2 accounts

--- dev/ganache-server.ts
+++ dev/ganache-server.ts
@@ -5,6 +5,7 @@ import { execIfScript } from './dev-utils'
 import { Config } from './config'
 
 export const serve = (c: Config) => {
+  const balance = '0xFFF FFF FFF FFF FFF FFF'.replace(/ /g, '')
   const options = {
     port: c.ethPort,
     hostname: c.hostname,
@@ -13,8 +14,11 @@ export const serve = (c: Config) => {
 
     db: (memdown as any)(), // todo: allow other options then in-memory, but make sure we do no run out of disk space
     locked: false,
-    mnemonic: 'dignity upset visa worry warrior donate record enforce time pledge ladder drop',
-    default_balance_ether: 1000000,
+    // mnemonic: 'dignity upset visa worry warrior donate record enforce time pledge ladder drop',
+    accounts: [
+      '0x6c7cfe3c8c47dc2ea38e2634f8a99ecea87b9609e888be36a2d7ee076d28bdce', // 7582C707b9990a5BB3Ca23f8F7b61B6209829A6e
+      '0xd365947df31e3f828e7572bcdbd50554a9043c30b785a0f8e5811c6bf93f628c'  // cF0eD51326d08281D46F1c3475c8d720Cc2680d2
+    ].map(pk => ({ secretKey: pk, balance })),
     gasPrice: 200,
     logger: console
   }
